33c0b6ebb8b1a12e8973e7b9f8aad0b7af76d118
zqq90 <zqq_90@163.com>
2013-12-13 13:21:20 +0800

    Update webit-script to v1.3.0

--- servlet-filter-demo/src/main/java/webit/script/demo/servlet/BooksServlet.java
+++ servlet-filter-demo/src/main/java/webit/script/demo/servlet/BooksServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import webit.script.demo.mvc.util.BookUtil;
-import webit.script.support.servlet.WebitScriptFilterUtil;
+import webit.script.support.servlet.WebitScriptServletUtil;
 
 /**
  * 
@@ -18,7 +18,7 @@ public class BooksServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        //WebitScriptFilterUtil.setTemplatePath(request, "/books.wtl");
+        //WebitScriptServletUtil.setTemplatePath(request, "/books.wtl");
         request.setAttribute("books", BookUtil.findBooks());
     }
 
